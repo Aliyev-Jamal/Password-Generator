c5349e504706390553c2686aab5a2a7665f51349
Aliyev-Jamal <dr.iradahajiyeva1958@gmail.com>
2025-05-23 13:59:44 +0400

    copy function and bag fix

--- js/index.js
+++ js/index.js
@@ -33,6 +33,7 @@ function initEvent() {
         const length = elements.lengthSlider.value
         config.length = length
         elements.lengthValue.textContent = length
+        generatePassword()
     })
 
     Object.keys(elements.checkboxes).forEach(type => {
@@ -46,7 +47,7 @@ function initEvent() {
 
     elements.generateBth.addEventListener('click', generatePassword)
 
-    // elements.copyBtn.addEventListener('click' copyPassword)
+    elements.copyBtn.addEventListener('click', copyPassword)
 }
 
 
@@ -54,7 +55,7 @@ function generatePassword() {
     const atLeastOneSelected = Object.values(config).some(value => typeof value === 'boolean' && value === true)
     if (!atLeastOneSelected) {
         elements.resultBox.textContent = 'Select at least one setting!'
-        // updateStrength(0)
+        updateStrength(0)
         return
     }
 
@@ -73,7 +74,7 @@ function generatePassword() {
     selectedTypes.forEach(type => {
         const charSet = charSets[type]
         const randomChar = charSet.charAt(Math.floor(Math.random() * charSet.length))
-        password + randomChar
+        password += randomChar
     })
 
     for (let i = selectedTypes.length; i < config.length; i++) {
@@ -84,7 +85,28 @@ function generatePassword() {
     password = shufleString(password)
 
     elements.resultBox.textContent = password
-    // updateStrength(calculateStength())
+    updateStrength(calculateStength())
+}
+
+function calculateStength(){
+    const { length, uppercase, lowercase, numbers, symbols} = config
+    let strength = Math.min(5, Math.floor(length / 6))
+
+    let varietyScore = 0
+    if(uppercase) varietyScore++
+    if(lowercase) varietyScore++
+    if(numbers) varietyScore++
+    if(symbols) varietyScore++
+
+    return Math.min(4, Math.floor((strength + varietyScore) / 2))
+}
+
+function updateStrength(strength){
+    const strengthLables = ['very easy', 'easy', 'medium', 'hard', 'very hard']
+    elements.strengthValue.textContent = strengthLables[strength]
+    
+    const colors = ['red', 'orange', 'yellow', 'greenyellow', 'lightgreen']
+    elements.strengthValue.style.color = colors[strength]
 }
 
 function shufleString(str) {
@@ -94,6 +116,25 @@ function shufleString(str) {
             ;[array[i], array[j]] = [array[j], array[i]]
     }
     return array.join('')
+}
+
+function copyPassword(){
+    const password = elements.resultBox.textContent
+
+    if(!password || password === 'Select at least one symbol!'){
+        return
+    }
+    navigator.clipboard
+        .writeText(password)
+        .then(() => {
+            const originalText = elements.copyBtn.innerHTML
+            elements.copyBtn.querySelector('img').src = `./assets/check.svg`
+
+            setTimeout(() => {
+                elements.copyBtn.querySelector('img').src = `./assets/copy.png`
+            }, 1500);
+
+        })
 }
 
 function init() {
